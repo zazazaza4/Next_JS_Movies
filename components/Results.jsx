002922fe339eff66df653d7d6d37acbac26f0bd2
zazazaza4 <90201472+zazazaza4@users.noreply.github.com>
2022-06-27 19:25:54 +0300

    Add files via upload

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,7 +1,7 @@
 import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
-const Results = ({ results }) => {
+const Results = ({ results = [] }) => {
   return (
     <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {
